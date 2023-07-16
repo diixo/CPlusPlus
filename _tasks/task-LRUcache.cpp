118f46822664eb9b59ad0ba2eb42a372d4e36386
diixo <anatolii.podoba@gmail.com>
2023-07-16 07:15:03 +0300

    Final impl LRUcache.

--- _tasks/task-LRUcache.cpp
+++ _tasks/task-LRUcache.cpp
@@ -8,6 +8,11 @@ struct item
 {
    int key;
    int value;
+
+   item() = default;
+   item(const item&) = default;
+   item& operator=(const item&) = default;
+
    ~item() { std::cout << "~dtor\n"; }
 };
 
@@ -42,26 +47,73 @@ public:
 
    void put(int key, int value)
    {
-      //
+      auto iter = mMap.find(key);
+
+      // push new item
+      if (iter == mMap.end())
+      {
+         if (mDeque.size() == mSize)
+         {
+            mMap.erase(mDeque.back().key);
+            mDeque.pop_back();
+         }
+         
+         mDeque.push_front(item{ key, value });
+         mMap[key] = mDeque.begin();
+      }
+      else
+      {
+         // rewrite exist item with new value
+         mDeque.erase(iter->second);
+         mDeque.push_front(item{ key, value });
+         iter->second = mDeque.begin();
+      }
    }
 
+   std::list <item>::const_iterator begin() const
+   {
+      return mDeque.begin();
+   }
+
+   std::list <item>::const_iterator end() const
+   {
+      return mDeque.end();
+   }
 };
 
-
-int main() 
+int main()
 {
    int value = 0;
 
+   LRUcache lru(3);
+   value = lru.get(0);
+
+   lru.put(1, 111);
+   lru.put(2, 222);
+   lru.put(3, 333);
+
+   value = lru.get(2);
+
+   lru.put(4, 444);
+
+   for (auto& i: lru)
+   {
+      printf("%d : %d\n", i.key, i.value);
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+
    LRUcache cache(2);
-   cache.put(1, 111);
-   cache.put(2, 222);
-   value = cache.get(1);       // returns 111
+   cache.put(1, 11);
+   cache.put(2, 22);
+   value = cache.get(1);       // returns 11
    cache.put(3, 33);           // evicts key 2
    value = cache.get(2);       // returns -1 (not found)
    cache.put(4, 44);           // evicts key 1
    value = cache.get(1);       // returns -1 (not found)
-   value = cache.get(3);       // returns 3
-   value = cache.get(4);       // returns 4
+   value = cache.get(3);       // returns 33
+   value = cache.get(4);       // returns 44
+
 
    return 0;
 }
